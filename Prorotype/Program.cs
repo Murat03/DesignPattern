1e8e9f272e1a5e3c33e6664a4589583c51501e6f
Murat03 <muratipekk033@gmail.com>
2023-04-08 17:07:03 +0300

    Builder Design

--- Prorotype/Program.cs
+++ Prorotype/Program.cs
@@ -10,6 +10,41 @@ namespace Prorotype
     {
         static void Main(string[] args)
         {
+            Customer customer1 = new Customer { City="Eskişehir",FirstName="Murat",LastName="İpek",Id=1};
+
+            //Person döndürür, başında Customerı belirtirsek customer döndürür.
+            Customer customer2 = (Customer)customer1.Clone();
+            customer2.FirstName = "Melih";
+            
+            Console.WriteLine(customer1.FirstName);
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class Customer: Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+    public class Employee : Person
+    {
+        public decimal Salary { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
         }
     }
 }
